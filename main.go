8d224308b286e65ad7b552d9776497278e548dab
Felipe <felipe.janazi@sptech.school>
2025-05-29 14:35:25 -0300

    feet: add custom logger

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 // Para fazer a instalação de todos pacotes que estão sendo utilizados e deltar os que não estão sendo podemos usar o:
 // go mod tidy
-import "github.com/Felipe-Janazi/gooportunities/router"
+import (
+	"github.com/Felipe-Janazi/gooportunities/config"
+	"github.com/Felipe-Janazi/gooportunities/router"
+)
 
-func main () {
-	
+var (
+	logger *config.Logger
+)
+
+func main() {
+
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	// Initialize router
 	router.Initialize()
 }
